93f0d33ee6b6ff878e1e014c24ac9ff32f46f689
Nathan M.S <natms24@gmail.com>
2024-10-21 19:33:43 +0300

    Add final version of lab 2

--- lab_2_distributed_task_scheduler/main.go
+++ lab_2_distributed_task_scheduler/main.go
@@ -13,9 +13,13 @@ type Task struct {
 	data string
 }
 
+// Total count of tasks
 const TASK_COUNT = 5
+
+// Limit of the number of times a process can be retried
 const RETRY_LIMIT = 1
 
+// Tasks currently completed
 var COMPLETED_TASKS = 0
 
 // Worker function that processes tasks. If a worker fails, the task will be sent to failChan.
@@ -23,18 +27,22 @@ func worker(id int, taskChan <-chan Task, failChan chan<- Task, wg *sync.WaitGro
 	defer wg.Done()
 
 	for task := range taskChan {
-		// fmt.Printf("Worker %d started task %d: %s\n", id, task.id, task.data)
+		fmt.Printf("\t - Worker %d started task %d: %s\n", id, task.id, task.data)
 
 		// Simulate random failure (30% chance of failure)
-		if rand.Float32() < 0.9 {
-			fmt.Printf("Worker %d failed on task %d\n", id, task.id)
+		if rand.Float32() < 0.3 {
+			fmt.Printf("\033[31m\t - Worker %d failed on task %d\033[0m\n", id, task.id)
 			failCounter[task.id] += 1
+
 			if failCounter[task.id] > RETRY_LIMIT {
-				fmt.Printf("Terminated task %d - exceeded retry limit of %d\n", task.id, RETRY_LIMIT)
+				fmt.Printf("\033[33m\t - Terminated task %d - exceeded retry limit of %d\033[0m\n", task.id, RETRY_LIMIT)
+
+				// Close the failed task channel if all the tasks have been completed
 				COMPLETED_TASKS += 1
 				if COMPLETED_TASKS == TASK_COUNT {
 					close(failChan)
 				}
+
 			} else {
 				failChan <- task
 			}
@@ -43,8 +51,9 @@ func worker(id int, taskChan <-chan Task, failChan chan<- Task, wg *sync.WaitGro
 		}
 
 		time.Sleep(time.Duration(rand.Intn(3)+1) * time.Second)
-		fmt.Printf("Worker %d completed task %d\n", id, task.id)
+		fmt.Printf("\033[32m\t - Worker %d completed task %d\033[0m\n", id, task.id)
 
+		// Close the failed task channel if all the tasks have been completed
 		COMPLETED_TASKS += 1
 		if COMPLETED_TASKS == TASK_COUNT {
 			close(failChan)
@@ -63,9 +72,13 @@ func main() {
 		{id: 5, data: "Task 5"},
 	}
 
+	// channels
 	taskChan := make(chan Task, len(tasks))
 	failChan := make(chan Task, len(tasks))
+
+	// map for keeping track of the failures
 	failCounter := make(map[int]int)
+
 	var wg sync.WaitGroup
 	workerCount := 3
 
@@ -80,11 +93,14 @@ func main() {
 
 	// Handle failed tasks by redistributing them
 	go func() {
+		currentWorkerID := 0
 		for failedTask := range failChan {
-			fmt.Printf("Reassigning failed task %d\n", failedTask.id)
+			fmt.Printf("\033[37m\t - Reassigning failed task %d to worker %d\033[0m\n", failedTask.id, currentWorkerID+1)
 			wg.Add(1)
 			taskChan <- failedTask
-			go worker(rand.Intn(workerCount)+1, taskChan, failChan, &wg, failCounter)
+			go worker(currentWorkerID+1, taskChan, failChan, &wg, failCounter)
+
+			currentWorkerID = (currentWorkerID + 1) % workerCount // cycle worker id
 		}
 
 		close(taskChan)
@@ -92,6 +108,7 @@ func main() {
 
 	// Wait for all workers to finish
 	wg.Wait()
+	time.Sleep(time.Second)
 
-	fmt.Println("All tasks completed.")
+	fmt.Println("\n\tAll tasks completed")
 }
